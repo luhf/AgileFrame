9ef66820b3b8370a9cd8d2973f4578d20b41cb95
luhf <g.luhaifeng@gmail.com>
2013-04-05 17:20:58 +0800

    公用的js脚本
    Signed-off-by: luhf <g.luhaifeng@gmail.com>

--- WebContent/res/js/base.js
+++ WebContent/res/js/base.js
@@ -1,9 +1,51 @@
-$(function() {
-	_AF.nav.initNav();//初始化导航菜单
-	_AF.tabs.initTab();//绑定右键菜单事件
-	_AF.theme.initTheme();//初始化切换主题
-});
+/**
+ * 公用方法
+ */
 var _AF={
+	/**
+	 * ajax
+	 */
+	ajax:{
+		/**
+		 * post提交
+		 * @param url 路径
+		 * @param param 参数
+		 * @param dataType 类型
+		 * @param callback 回调函数
+		 * @example _AF.ajax.post('/xxx/xxx/xxx',{a:1,b:2},'json',function(html){//处理返回结果});
+		 */
+		post:function(url,param,dataType,callback){
+			$.ajax({
+				type:'POST',
+				url:url,
+				dataType:dataType,
+				data:param,
+				success:callback,
+				error:function(html){
+					alert("提交数据失败，代码:" +html.status+ "，请稍候再试");
+				}
+			});
+		},
+		/**
+		 * post提交并刷新页面
+		 * @param url 路径
+		 * @param param 参数
+		 * @param dataType 类型
+		 * @example _AF.ajax.post('/xxx/xxx/xxx',{a:1,b:2},'json');
+		 */
+		post_refresh:function(url,param,dataType,callback){
+			$.ajax({
+				type:'POST',
+				url:url,
+				dataType:dataType,
+				data:param,
+				success:function(html){location.reload();},
+				error:function(html){
+					alert("提交数据失败，代码:" +html.status+ "，请稍候再试");
+				}
+			});
+		}
+	},
 	
 	/**
 	 * cookie
@@ -46,44 +88,51 @@ var _AF={
 	 */
 	theme:{
 		/**
+		 * 所有主题资源
+		 */
+		themes:{
+			'gray' : '/res/css/themes/gray/easyui.css',
+			'black' : '/res/css/themes/black/easyui.css',
+			'bootstrap' : '/res/css/themes/bootstrap/easyui.css',
+			'blue' : '/res/css/themes/default/easyui.css',
+			'metro' : '/res/css/themes/metro/easyui.css'
+		},
+		/**
 		 * 初始化主题
 		 */
-		initTheme:function(){
-			var themes = {
-				'gray' : '/res/css/themes/gray/easyui.css',
-				'black' : '/res/css/themes/black/easyui.css',
-				'bootstrap' : '/res/css/themes/bootstrap/easyui.css',
-				'blue' : '/res/css/themes/default/easyui.css',
-				'metro' : '/res/css/themes/metro/easyui.css'
-			};
-			_AF.theme.cookieTheme(themes);
+		init:function(){
+			_AF.theme.getCookieTheme();
 			var skins = $('.li-skinitem span').click(function() {
-				_AF.theme.setTheme(skins, this, themes);
+				_AF.theme.setCookieTheme(skins, this);
 			});
 		},
 		/**
 		 * 设置主题
+		 */
+		setTheme:function(skin){
+			$('#swicth-style').attr('href', _AF.theme.themes[skin]);
+		},
+		/**
+		 * Cookie设置主题
 		 * @param skins 所有主题selector
 		 * @param current 当前主题
-		 * @param themes 所有主题资源
 		 */
-		setTheme:function(skins,current,themes){
+		setCookieTheme:function(skins,current){
 			var $this = $(current);
 			if($this.hasClass('cs-skin-on')) return;
 			skins.removeClass('cs-skin-on');
 			$this.addClass('cs-skin-on');
 			var skin = $this.attr('rel');
-			$('#swicth-style').attr('href', themes[skin]);
+			_AF.theme.setTheme(skin);
 			_AF.cookie.setCookie('cs-skin', skin, {})
 		},
 		/**
-		 * 从cookie中获取并设置主题
-		 * @param themes 所有主题资源
+		 * Cookie获取主题
 		 */
-		cookieTheme:function(themes){
+		getCookieTheme:function(){
 			if(_AF.cookie.getCookie('cs-skin')) {
 				var skin = _AF.cookie.getCookie('cs-skin');
-				$('#swicth-style').attr('href', themes[skin]);
+				_AF.theme.setTheme(skin);
 				$this = $('.li-skinitem span[rel='+skin+']');
 				$this.addClass('cs-skin-on');
 			}
@@ -95,68 +144,9 @@ var _AF={
 	 */
 	tabs:{
 		/**
-		 * 返回创建的iframe
-		 * @param url 页面路径
-		 */
-		createFrame:function(url){
-			var s = '<iframe scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
-			return s;
-		},
-		
-		/**
-		 * 创建tabs的iframe
-		 */
-		addTab:function(title,url){
-			if ($('#tabs').tabs('exists', title)){
-				$('#tabs').tabs('select', title);//选中并刷新
-				var currTab = $('#tabs').tabs('getSelected');
-				var url = $(currTab.panel('options').content).attr('src');
-				if(url != undefined && currTab.panel('options').title != '首页(Home)') {
-					$('#tabs').tabs('update',{
-						tab:currTab,
-						options:{
-							content:_AF.tabs.createFrame(url)
-						}
-					})
-				}
-			} else {
-				$('#tabs').tabs('add',{
-					title:title,
-					content:_AF.tabs.createFrame(url),
-					closable:true
-				});
-			}
-			_AF.tabs.tabClose();
-		},
-		
-		/**
-		 * tabs关闭
-		 */
-		tabClose:function(){
-			/*双击关闭TAB选项卡*/
-			$(".tabs-inner").dblclick(function(){
-				var subtitle = $(this).children(".tabs-closable").text();
-				$('#tabs').tabs('close',subtitle);
-			})
-			/*为选项卡绑定右键*/
-			$(".tabs-inner").bind('contextmenu',function(e){
-				$('#mm').menu('show', {
-					left: e.pageX,
-					top: e.pageY
-				});
-
-				var subtitle =$(this).children(".tabs-closable").text();
-
-				$('#mm').data("currtab",subtitle);
-				$('#tabs').tabs('select',subtitle);
-				return false;
-			});
-		},
-		
-		/**
 		 * 绑定右键菜单
 		 */
-		initTab:function(){
+		init:function(){
 			//刷新
 			$('#mm-tabupdate').click(function(){
 				var currTab = $('#tabs').tabs('getSelected');
@@ -165,7 +155,7 @@ var _AF={
 					$('#tabs').tabs('update',{
 						tab:currTab,
 						options:{
-							content:_AF.tabs.createFrame(url)
+							content:_AF.tabs.create(url)
 						}
 					})
 				}
@@ -210,13 +200,14 @@ var _AF={
 			$('#mm-tabcloseright').click(function(){
 				var nextall = $('.tabs-selected').nextAll();
 				if(nextall.length==0){
-					//msgShow('系统提示','后边没有啦~~','error');
-					alert('后边没有啦~~');
+					_AF.msg.show('系统提示', '右侧没有可关闭的tab页', 'error');
 					return false;
 				}
 				nextall.each(function(i,n){
 					var t=$('a:eq(0) span',$(n)).text();
-					$('#tabs').tabs('close',t);
+					if(t != '首页(Home)'){
+						$('#tabs').tabs('close',t);
+					}
 				});
 				return false;
 			});
@@ -224,13 +215,76 @@ var _AF={
 			$('#mm-tabcloseleft').click(function(){
 				var prevall = $('.tabs-selected').prevAll();
 				if(prevall.length==0){
-					alert('到头了，前边没有啦~~');
+					_AF.msg.show('系统提示', '左侧没有可关闭的tab页', 'error');
+					return false;
+				}else if(prevall.length==1){
+					_AF.msg.show('系统提示', '首页不能被关闭', 'error');
 					return false;
 				}
 				prevall.each(function(i,n){
 					var t=$('a:eq(0) span',$(n)).text();
-					$('#tabs').tabs('close',t);
+					if(t != '首页(Home)'){
+						$('#tabs').tabs('close',t);
+					}
 				});
+				return false;
+			});
+		},
+		/**
+		 * 返回创建的iframe
+		 * @param url 页面路径
+		 */
+		create:function(url){
+			var s = '<iframe scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
+			return s;
+		},
+		
+		/**
+		 * 创建tabs的iframe
+		 */
+		add:function(title,url){
+			if ($('#tabs').tabs('exists', title)){
+				$('#tabs').tabs('select', title);//选中并刷新
+				var currTab = $('#tabs').tabs('getSelected');
+				var url = $(currTab.panel('options').content).attr('src');
+				if(url != undefined && currTab.panel('options').title != '首页(Home)') {
+					$('#tabs').tabs('update',{
+						tab:currTab,
+						options:{
+							content:_AF.tabs.create(url)
+						}
+					})
+				}
+			} else {
+				$('#tabs').tabs('add',{
+					title:title,
+					content:_AF.tabs.create(url),
+					closable:true
+				});
+			}
+			_AF.tabs.close();
+		},
+		
+		/**
+		 * tabs关闭
+		 */
+		close:function(){
+			/*双击关闭TAB选项卡*/
+			$(".tabs-inner").dblclick(function(){
+				var subtitle = $(this).children(".tabs-closable").text();
+				$('#tabs').tabs('close',subtitle);
+			})
+			/*为选项卡绑定右键*/
+			$(".tabs-inner").bind('contextmenu',function(e){
+				$('#mm').menu('show', {
+					left: e.pageX,
+					top: e.pageY
+				});
+
+				var subtitle =$(this).children(".tabs-closable").text();
+
+				$('#mm').data("currtab",subtitle);
+				$('#tabs').tabs('select',subtitle);
 				return false;
 			});
 		}
@@ -240,12 +294,12 @@ var _AF={
 	 * 初始化导航菜单
 	 */
 	nav:{
-		initNav:function(){
+		init:function(){
 			$('.cs-navi-menu').click(function() {
 				var $this = $(this);
 				var href = $this.attr('src');
 				var title = $this.text();
-				_AF.tabs.addTab(title, href);
+				_AF.tabs.add(title, href);
 			});
 		}
 	},
